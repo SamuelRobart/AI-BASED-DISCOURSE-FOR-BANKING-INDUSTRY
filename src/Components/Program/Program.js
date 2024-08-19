c9b628cdb2358263cfa021842ab70ff5e81a4c5d
SamuelRobart <samuelolivarobart2000@gmail.com>
2024-08-19 14:35:56 +0530

    First commit

--- src/Components/Program/Program.js
+++ src/Components/Program/Program.js
@@ -4,8 +4,7 @@ import program_1 from '../../assets/program-1.png'
 import program_2 from '../../assets/program-2.png'
 import program_3 from '../../assets/program-3.png'
 import program_icon_1 from '../../assets/program-icon-1.png'
-import program_icon_2 from '../../assets/program-icon-2.png'
-import program_icon_3 from '../../assets/program-icon-3.png'
+
 
 const Program = () => {
   return (
